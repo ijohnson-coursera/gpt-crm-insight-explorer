5623795e08280722f7917e8729482b095b7052a9
ijohnson <ijohnson@coursera.org>
2023-05-09 13:41:55 -0400

    added mask names

--- remove_pii.py
+++ remove_pii.py
@@ -9,39 +9,39 @@ nlp = spacy.load("en_core_web_sm")
 def remove_pii(text):
     # Use spaCy NER to identify entities
     doc = nlp(text)
-    entity_types_to_remove = {"FAC", "PERSON", "GPE", "DATE", "ORG"}
+    entity_types_to_remove = {"FAC" : "MASKED_FAC", "PERSON" : "MASKED_PERSON", "GPE" : "MASKED_GPE", "DATE" : "MASKED_DATE", "ORG" : "MASKED_ORG"}
     text_parts = []
     start = 0
 
     for ent in doc.ents:
-        if ent.label_ in entity_types_to_remove:
+        if ent.label_ in entity_types_to_remove.keys():
             text_parts.append(text[start:ent.start_char])
-            text_parts.append("[REDACTED]")
+            text_parts.append(f"[{entity_types_to_remove[ent.label_]}]")
             start = ent.end_char
 
     text_parts.append(text[start:])
     text = "".join(text_parts)
 
     # Use regex patterns for other PII fields
-    pii_patterns = [
+    pii_patterns = {
         # Email address
-        r"[\w\.-]+@[\w\.-]+\.\w+",
-        # Phone number
-        r"\+?(\d[\s-]?){10,15}",
+        r"[\w\.-]+@[\w\.-]+\.\w+" : "MASKED_EMAIL_ADDRESS",
+        # Phone number 
+        r"\+?(\d[\s-]?){10,15}" : "MASKED_PHONE_NUMBER",
         # Social Security Number (SSN)
-        r"\d{3}-\d{2}-\d{4}",
+        r"\d{3}-\d{2}-\d{4}" : "MASKED_SOCIAL_SECIRITY_NUMBER",
         # Passport number (generic pattern, passport numbers vary by country)
-        r"[A-Z]{1,2}\d{6,10}",
+        r"[A-Z]{1,2}\d{6,10}" : "MASKED_PASSPORT_NUMBER",
         # Credit card number
-        r"\d{4}[\s-]?\d{4}[\s-]?\d{4}[\s-]?\d{4}",
+        r"\d{4}[\s-]?\d{4}[\s-]?\d{4}[\s-]?\d{4}" : "MASKED_CREDIT_CARD_NUMBER",
         # IP address
-        r"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)",
+        r"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" : "MASKED_IP_ADDRESS",
         # Date of birth
-        r"\d{1,2}[-/]\d{1,2}[-/]\d{2,4}",
-    ] 
+        r"\d{1,2}[-/]\d{1,2}[-/]\d{2,4}" : "MASKED_DATE",
+    } 
 
-    for pattern in pii_patterns:
-        text = re.sub(pattern, "[REDACTED]", text)
+    for pattern in pii_patterns.keys():
+        text = re.sub(pattern, f"[{pii_patterns[pattern]}]", text)
 
     return text
 
